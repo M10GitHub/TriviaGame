aec9d96d3f6fc9a2b02164530b5ba8c565ef3cfc
Martin Aguirre <martinaguirre@Martins-MacBook-Air.local>
2017-12-07 17:09:16 -0800

    commit before timer

---
+++ assets/javascript/app.js
@@ -0,0 +1,59 @@
+$("#a1").click(function(){
+	alert("Not quite...");
+});
+
+$("#b1").click(function(){
+	alert("No... but stay a while and listen!");
+});
+
+$("#c1").click(function(){
+	alert("Correct!");
+});
+
+$("#a2").click(function(){
+	alert("Fortunately that is incorrect");
+});
+
+$("#b2").click(function(){
+	alert("Correct!");
+});
+
+$("#c2").click(function(){
+	alert("Not quite...");
+});
+
+$("#a3").click(function(){
+	alert("Correct!");
+});
+
+$("#b3").click(function(){
+	alert("Incorrect, but still call them");
+});
+
+$("#c3").click(function(){
+	alert("Same here, but it is incorrect");
+});
+
+$("#a4").click(function(){
+	alert("Incorrect, but it is an large contributor");
+});
+
+$("#b4").click(function(){
+	alert("Correct!");
+});
+
+$("#c4").click(function(){
+	alert("Not Quite...");
+});
+
+$("#a5").click(function(){
+	alert("Not Quite...");
+});
+
+$("#b5").click(function(){
+	alert("Incorrect");
+});
+
+$("#c5").click(function(){
+	alert("Correct!");
+});
